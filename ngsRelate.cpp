51619f51103f18a3fb007d07b24d78a21820124b
krishang <kristianhanghoej@gmail.com>
2018-08-31 17:06:38 +0200

    changed it to the number of sites analyzed

--- ngsRelate.cpp
+++ ngsRelate.cpp
@@ -1537,11 +1537,11 @@ int main(int argc, char **argv){
         pthread_join(threads[i], NULL);
         worker_args * td_out_inbred = &all_args_inbred[cnt_inbred + i];
         if(td_out_inbred->best==0)
-          fprintf(stdout,"%d\t%f\t%f\t%f\t%d\t%f\t%d\n",td_out_inbred->a, p10[0],p10[1],td_out_inbred->bestll,-1,((double)td_out_inbred->nkeep)/((double)freq.size()), freq.size());
+          fprintf(stdout,"%d\t%f\t%f\t%f\t%d\t%f\t%d\n",td_out_inbred->a, p10[0],p10[1],td_out_inbred->bestll,-1,((double)td_out_inbred->nkeep)/((double)freq.size()), td_out_inbred->nkeep);
         if(td_out_inbred->best==1)
-          fprintf(stdout,"%d\t%f\t%f\t%f\t%d\t%f\t%d\n",td_out_inbred->a,p01[0],p01[1],td_out_inbred->bestll,-1,((double)td_out_inbred->nkeep)/((double)freq.size()), freq.size());
+          fprintf(stdout,"%d\t%f\t%f\t%f\t%d\t%f\t%d\n",td_out_inbred->a,p01[0],p01[1],td_out_inbred->bestll,-1,((double)td_out_inbred->nkeep)/((double)freq.size()), td_out_inbred->nkeep);
         if(td_out_inbred->best==2)
-          fprintf(stdout,"%d\t%f\t%f\t%f\t%d\t%f\t%d\n",td_out_inbred->a,td_out_inbred->pars[0],td_out_inbred->pars[1],td_out_inbred->bestll,td_out_inbred->niter,((double)td_out_inbred->nkeep)/((double)freq.size()), freq.size());
+          fprintf(stdout,"%d\t%f\t%f\t%f\t%d\t%f\t%d\n",td_out_inbred->a,td_out_inbred->pars[0],td_out_inbred->pars[1],td_out_inbred->bestll,td_out_inbred->niter,((double)td_out_inbred->nkeep)/((double)freq.size()), td_out_inbred->nkeep);
       fflush(stdout);
       }
       cnt_inbred += nTimes_inbred;
