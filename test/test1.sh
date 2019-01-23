1b2b1f31e33371a4302afa70ea43fff875a93d99
Thorfinn <thorfinn.sand@gmail.com>
2019-01-23 22:41:10 +0100

    added testscenarios along with md5sum

--- test/test1.sh
+++ test/test1.sh
@@ -14,19 +14,30 @@ fi
 LOG=${0}.log
 ODIR=odir
 echo "Cleaning old output dir ${ODIR} PRG:${PRG}" >${LOG}
-rm -rf ${ODIR} ${LOG}
+echo "rm -rf ${ODIR} ${LOG}"
 mkdir -p ${ODIR}
 
 gunzip -c small.bed.gz >small.bed
 
 echo "1) part1" >>${LOG}
-${PRG} -h small.vcf.gz -T GT -p 1 -c 1 -r 100 >${ODIR}/vcf.res 2>>${LOG} 
-${PRG} -h small.bcf -T GT -p 1 -c 1 -r 100 >${ODIR}/vcf2.res 2>>${LOG} 
 
+for c in 1 2 ##with gc
+do
+    ${PRG} -h small.vcf.gz -T GT -p 1 -c ${c} -r 100 >${ODIR}/vcf1.c${c}.res 2>>${LOG} 
+    ${PRG} -h small.bcf    -T GT -p 1 -c ${c} -r 100 >${ODIR}/vcf2.c${c}.res 2>>${LOG} 
+    ${PRG} -h small.vcf.gz       -p 1 -c ${c} -r 100 >${ODIR}/vcf3.c${c}.res 2>>${LOG} 
+    ${PRG} -h small.bcf          -p 1 -c ${c} -r 100 >${ODIR}/vcf4.c${c}.res 2>>${LOG} 
+    ${PRG} -P small.bed          -p 1 -c ${c} -r 100 >${ODIR}/vcf5.c${c}.res 2>>${LOG} 
+done
+
+for c in 0 ##without gc
+do
+    ${PRG} -h small.vcf.gz       -p 1 -c ${c} -r 100 >${ODIR}/vcf6.c${c}.res 2>>${LOG} 
+    ${PRG} -h small.bcf          -p 1 -c ${c} -r 100 >${ODIR}/vcf7.c${c}.res 2>>${LOG} 
+done
 
 echo -e "\t test all done "  >>${LOG} 2>&1
 
+#md5sum odir/* >test1.md5
 
-##when generated the results:
-##md5sum 
-#${MD5}  -c tajima/md5/pestPG.md5sum >>${LOG} 2>&1 || exit 1
+${MD5}  -c test1.md5 >>${LOG} 2>&1 || exit 1
