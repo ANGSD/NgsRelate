8035a68c8b3841a0ce6828b48c77620ebcdf3bd9
Thorfinn <thorfinn.sand@gmail.com>
2019-01-25 16:07:52 +0100

    added testscript for R

---
+++ R/compare.results.R
@@ -0,0 +1,8 @@
+x<-read.table("test/odir/vcf8.res",he=T)
+y<-read.table("test/odirworks2/vcf8.res",he=T)
+
+x<-read.table("test/odir/vcf1.c1.res",he=T)
+y<-read.table("test/odirworks2/vcf1.c1.res",he=T)
+
+
+
